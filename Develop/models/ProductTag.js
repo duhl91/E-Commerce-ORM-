aec7015487a9ee482ee1cdd428ddc66b4558916b
dalton uhl <dalton.uhl@yahoo.com>
2021-11-02 09:37:33 -0400

    defined product and tag columns

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -7,6 +7,29 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER, 
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    //product_id column
+    product_id: {
+      type: DataTypes.INTEGER,
+      // references the product model's id 
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    //tag_id column
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
